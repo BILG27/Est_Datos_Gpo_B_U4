29fc9d981c2747ef3a3d365fc83bc307aa416486
invitado <invitado@LAB020212.itchii.lab>
2022-04-26 14:44:55 -0600

    Prueba SO Siguiente prueba

--- src/A/ArbolBinarioA.java
+++ src/A/ArbolBinarioA.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+se utiliza ejercicio para repositorio "Est_Datos_GPO_B_U4" en GitHub
  */
 package A;
 
